fb74a9306a139c91462bec531e384236f12c247e
unknown <starkliew@gmail.com>
2019-11-13 10:35:52 +0800

    form route

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Sign Up') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="/register">
                         @csrf
 
                         <div class="form-group row">
