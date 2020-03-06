02f091c3eddacf1bedf0721ee3c69ce608bb5bcf
unknown <starkliew@gmail.com>
2020-03-06 13:34:22 +0800

    fixed bug in routes.js

--- resources/backoffice/components/admin/product/Variant.vue
+++ resources/backoffice/components/admin/product/Variant.vue
@@ -147,7 +147,6 @@
 import { mapGetters } from 'vuex'
 import Crud from '../shared/Crud'
 import AvatarCropper from 'vue-avatar-cropper'
-import Vue from 'Vue'
 import { swatches } from '~~/config'
 
 export default {
